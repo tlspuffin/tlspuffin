d3ddbb8e516f2da538950b998e47f53407d478ac
Maximilian Ammann <max@maxammann.org>
2021-06-15 16:04:07 +0200

    Fix coverage script

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -3,9 +3,13 @@
 WORK_DIR="./target-nightly"
 TARGET_DIR="$WORK_DIR"
 
+# This only work with the Rust nightly address sanitizer.
+# For some reason the asan runtime we link in openssl-src is not enough!
+
 # We also need asan in linker: https://stackoverflow.com/questions/42482494/undefined-reference-to-asan-init-v4-when-compiling
+# We enabled asan in openssl and also link libasan by default.
 # Disable features such that __sanitizer_cov_trace_pc_guard* is not implemented. It gets implemented by the address
-# sanitizer.s
+# sanitizer.
 # Adapted from https://github.com/rust-fuzz/libfuzzer
 cargo +nightly rustc --example seed_successful \
     --target-dir "$TARGET_DIR" \
@@ -17,11 +21,9 @@ cargo +nightly rustc --example seed_successful \
 #export RUSTFLAGS="-Z sanitizer=address"
 
 export ASAN_OPTIONS="coverage=1:coverage_dir=$WORK_DIR"
-# Run seed_successful as an example
-#cargo +nightly run --example seed_successful --no-default-features --target-dir "$TARGET_DIR"
 
-BIN="$TARGET_DIR/debug/examples/seed_successful"
-SERVER="$(pwd)/tools/coverage-report-server.py"
+BIN="$TARGET_DIR/x86_64-unknown-linux-gnu/debug/examples/seed_successful"
+
 
 rm "$WORK_DIR/"*.sancov
 
@@ -32,4 +34,6 @@ sancov --print-coverage-stats "$WORK_DIR/"*.sancov "$BIN"
 
 # Serve HTML
 sancov -symbolize "$WORK_DIR/"*.sancov "$BIN" > "$WORK_DIR/symbolized.symcov"
+
+SERVER="$(pwd)/tools/coverage-report-server.py"
 python "$SERVER" --symcov "$WORK_DIR/symbolized.symcov" --srcpath dummy
