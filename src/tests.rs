1040e8436427cbc14730b08094fc11485d0de8e7
Maximilian Ammann <max@maxammann.org>
2021-06-16 11:31:56 +0200

    add test for parsing client hello

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 pub mod tlspuffin {
     use nix::sys::signal::Signal;
-    use nix::sys::wait::WaitStatus::{Signaled, Exited};
+    use nix::sys::wait::WaitStatus::{Exited, Signaled};
     use nix::sys::wait::{waitpid, WaitPidFlag};
     use nix::unistd::{fork, ForkResult};
     use test_env_log::test;
@@ -150,6 +150,8 @@ pub mod tlspuffin {
 
 #[cfg(test)]
 pub mod integration {
+    use std::any::{Any, TypeId};
+    use std::borrow::Borrow;
     use std::convert::TryFrom;
     use std::{
         io::{stdout, Read, Write},
@@ -174,19 +176,17 @@ pub mod integration {
         Connection, ProtocolVersion, RootCertStore,
     };
     use test_env_log::test;
+    use tls_parser::{parse_tls_plaintext, TlsMessage, TlsMessageHandshake};
     use webpki;
     use webpki_roots;
 
     use crate::agent::AgentName;
     use crate::fuzzer::seeds::{seed_client_attacker, seed_client_attacker12};
+    use crate::variable_data::AsAny;
     use crate::{
         fuzzer::seeds::seed_successful,
         trace::{Trace, TraceContext},
     };
-    use tls_parser::{TlsMessage, TlsMessageHandshake, parse_tls_plaintext};
-    use std::any::{TypeId, Any};
-    use crate::variable_data::AsAny;
-    use std::borrow::Borrow;
 
     #[test]
     fn test_serialisation_seed_client_attacker_json() {
@@ -261,6 +261,40 @@ pub mod integration {
         5ab4cdf01b04e9ffc0fc50";
 
         let hello_client = hex::decode(hello_client_hex).unwrap();
+        hexdump::hexdump(&hello_client);
+
+        let opaque_message =
+            OpaqueMessage::read(&mut Reader::init(hello_client.as_slice())).unwrap();
+        println!("{:#?}", Message::try_from(opaque_message).unwrap());
+    }
+
+    #[test]
+    fn test_heartbleed_ch() {
+        // Derived from "openssl s_client -msg -connect localhost:44330" and then pressing R
+        let hello_client_hex = "
+            16 03 03 00  dc 01 00 00 d8 03 03 53
+            43 5b 90 9d 9b 72 0b bc  0c bc 2b 92 a8 48 97 cf
+            bd 39 04 cc 16 0a 85 03  90 9f 77 04 33 d4 de 00
+            00 66 c0 14 c0 0a c0 22  c0 21 00 39 00 38 00 88
+            00 87 c0 0f c0 05 00 35  00 84 c0 12 c0 08 c0 1c
+            c0 1b 00 16 00 13 c0 0d  c0 03 00 0a c0 13 c0 09
+            c0 1f c0 1e 00 33 00 32  00 9a 00 99 00 45 00 44
+            c0 0e c0 04 00 2f 00 96  00 41 c0 11 c0 07 c0 0c
+            c0 02 00 05 00 04 00 15  00 12 00 09 00 14 00 11
+            00 08 00 06 00 03 00 ff  01 00 00 49 00 0b 00 04
+            03 00 01 02 00 0a 00 34  00 32 00 0e 00 0d 00 19
+            00 0b 00 0c 00 18 00 09  00 0a 00 16 00 17 00 08
+            00 06 00 07 00 14 00 15  00 04 00 05 00 12 00 13
+            00 01 00 02 00 03 00 0f  00 10 00 11 00 23 00 00
+            00 0f 00 01 01";
+
+        let hello_client = hex::decode(
+            hello_client_hex
+                .to_string()
+                .replace(" ", "")
+                .replace("\n", ""),
+        )
+        .unwrap();
         hexdump::hexdump(&hello_client);
 
         let opaque_message =
@@ -411,14 +445,15 @@ pub mod integration {
     }
 
     fn a<'a>(data: &'a [u8]) {
-        let message1: TlsMessage<'a> = TlsMessage::Handshake(TlsMessageHandshake::CertificateVerify(data));
+        let message1: TlsMessage<'a> =
+            TlsMessage::Handshake(TlsMessageHandshake::CertificateVerify(data));
         let id = TypeId::of::<TlsMessage>();
         // let any = message1.as_any();
         //assert_eq!(id, message1.type_id()); not possible because data is not 'static
     }
 
     struct StaticTlsMessage {
-        data: Vec<u8>
+        data: Vec<u8>,
     }
 
     trait AsTlsMessage<'a> {
@@ -435,8 +470,8 @@ pub mod integration {
 
     #[test]
     fn test_tlsparser() {
-
-        let message: TlsMessage<'static> = TlsMessage::Handshake(TlsMessageHandshake::EndOfEarlyData);
+        let message: TlsMessage<'static> =
+            TlsMessage::Handshake(TlsMessageHandshake::EndOfEarlyData);
 
         let id = TypeId::of::<TlsMessage>();
 
