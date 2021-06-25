dbc06131289bcbd651303997003769ac70570d66
Maximilian Ammann <max@maxammann.org>
2021-06-25 11:21:54 +0200

    Fix analyze-crashes.sh

--- tools/analyze-crashes.sh
+++ tools/analyze-crashes.sh
@@ -3,4 +3,4 @@
 #rsync -r "mammann@pesto-calc.loria.fr:/local-homes/mammann/tlspuffin/experiments" .
 crash_dir="./experiments/$1/crashes"
 find "$crash_dir" -name "*.trace" -exec sh -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin execute $1 2>$1.log' _ {} \;
-python3 tools/asanalyzer.py -d 3 "'$crash_dir'"
+python3 tools/asanalyzer.py -d 3 "'$crash_dir/*.log'"
