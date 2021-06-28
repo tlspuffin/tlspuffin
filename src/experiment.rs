df57fe70c5baa5424e9b3d3f9d1e91935527b880
Maximilian Ammann <max@maxammann.org>
2021-06-28 09:51:31 +0200

    Add commit msg

--- src/experiment.rs
+++ src/experiment.rs
@@ -15,23 +15,31 @@ pub fn get_git_ref() -> Result<String, io::Error> {
     Ok(String::from_utf8(output.stdout).unwrap_or("unknown".to_string()).trim().to_string())
 }
 
+pub fn get_git_msg() -> Result<String, io::Error> {
+    let output = Command::new("git").args(&["log", "-i", "--pretty=%B"]).output()?;
+    Ok(String::from_utf8(output.stdout).unwrap_or("unknown".to_string()).trim().to_string())
+}
+
 pub fn write_experiment_markdown(
     directory: &PathBuf,
     title: impl Display,
     description_text: impl Display,
 ) -> Result<String, io::Error> {
     let git_ref = get_git_ref()?;
+    let git_msg = get_git_msg()?;
     let full_description = format!(
         "# Experiment: {title}\n\
                 * OpenSSL: {openssl_version}\n\
                 * Date: {date}\n\
                 * Git Ref: {git_ref}\n\
+                * Git Commit: {git_msg}\n\
                 * Log: [tlspuffin-log.json](./tlspuffin-log.json)\n\n\
                 {description}\n",
         title = &title,
         openssl_version = openssl_binding::openssl_version(),
         date = Utc::now().to_rfc3339(),
         git_ref = git_ref,
+        git_msg = git_msg,
         description = description_text
     );
 
