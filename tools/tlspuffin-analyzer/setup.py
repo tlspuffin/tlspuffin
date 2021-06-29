4f5be8fda76a2a3d117d516b4908b71683428cd6
Maximilian Ammann <max@maxammann.org>
2021-06-29 11:08:15 +0200

    Add python analyzer

---
+++ tools/tlspuffin-analyzer/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='tlspuffin-analyzer',
+    version='0.1',
+    packages=['analyzer'],
+    url='',
+    license='',
+    author='max',
+    author_email='max@maxammann.org',
+    description='',
+    scripts=['tlspuffin-analyzer'],
+    install_requires=[
+        'jsonslicer',
+        'matplotlib',
+        'numpy',
+    ],
+)
