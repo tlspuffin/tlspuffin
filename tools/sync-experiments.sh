62f089b8f34ec5ada30cd3be595884230481f2df
Maximilian Ammann <max@maxammann.org>
2021-06-30 12:38:59 +0200

    Ignore logs

--- tools/sync-experiments.sh
+++ tools/sync-experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tlspuffin="$(dirname "$0")/.."
-rsync --delete --progress -r "mammann@pesto-calc.loria.fr:/local-homes/mammann/tlspuffin/experiments/." "$tlspuffin/experiments-pesto/"
-rsync --delete --progress -r "max@cassis-calc.loria.fr:/home/max/tlspuffin/experiments/." "$tlspuffin/experiments-cassis/"
+rsync --delete --exclude 'tlspuffin-log.json' --progress -r "mammann@pesto-calc.loria.fr:/local-homes/mammann/tlspuffin/experiments/." "$tlspuffin/experiments-pesto/"
+rsync --delete --exclude 'tlspuffin-log.json' --progress -r "max@cassis-calc.loria.fr:/home/max/tlspuffin/experiments/." "$tlspuffin/experiments-cassis/"
