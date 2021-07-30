2f0c8cb42c8cb11021a4016eb40de797ddd59db1
Maximilian Ammann <max@maxammann.org>
2021-07-30 11:48:47 +0200

    Add tools for profiling rust

--- tools/profile/perf.sh
+++ tools/profile/perf.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-tlspuffin="$(dirname "$0")/.."
+tlspuffin="$(dirname "$0")/../.."
 
-perf record --call-graph dwarf -- cargo bench --bench benchmark -- --profile-time 1 mutations
-perf script | tools/stackcollapse-perf.pl | tools/rust-unmangle | tools/flamegraph.pl > flame.svg
+# bench="mutations"
+bench="seeds/seed_session_resumption_dhe"
+#bench="seeds/seed_client_attacker"
+
+perf record --call-graph dwarf -- cargo bench --bench benchmark -- --profile-time 1 $bench
+perf script | "$tlspuffin/tools/profile/stackcollapse-perf.pl" \
+            | "$tlspuffin/tools/profile/rust-unmangle" \
+            | "$tlspuffin/tools/profile/flamegraph.pl" > flame.svg
+
+xdg-open flame.svg
