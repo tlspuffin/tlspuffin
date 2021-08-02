a52954b4144218e9f26925e3469a08a5290f5843
Maximilian Ammann <max@maxammann.org>
2021-08-02 11:34:36 +0200

    Fix plot script

--- tools/plot-corpus.sh
+++ tools/plot-corpus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-find "corpus" -name "*.trace" -exec bash -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin plot --tree $1 svg plots/$(basename $1).svg' _ {} \;
+find "corpus" -name "*.trace" -exec bash -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin plot --tree $1 svg plots/$(basename $1)' _ {} \;
