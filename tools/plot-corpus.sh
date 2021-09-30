eb23a6bb717ae8cc529d0e0597cada3ca8b7b152
Maximilian Ammann <max@maxammann.org>
2021-09-30 12:06:04 +0200

    Adjust plot command

--- tools/plot-corpus.sh
+++ tools/plot-corpus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-find "corpus" -name "*.trace" -exec bash -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin plot --tree $1 svg plots/$(basename $1)' _ {} \;
+#find "corpus" -name "*.trace" -exec bash -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin plot --tree $1 svg plots/$(basename $1)' _ {} \;
 
-# find "corpus" -name "*.trace" -exec bash -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin plot --multiple $1 svg plots/$(basename $1)' _ {} \;
+find "corpus" -name "*.trace" -exec bash -c 'target/x86_64-unknown-linux-gnu/debug/tlspuffin plot --multiple $1 svg plots/$(basename $1)' _ {} \;
