bcf239a88b46d22cfe74ea65fca218f57dc77762
Maximilian Ammann <max@maxammann.org>
2022-07-06 19:15:48 +0200

    Add eve certs

--- tlspuffin/src/static_certs.rs
+++ tlspuffin/src/static_certs.rs
@@ -9,17 +9,19 @@
 //! ```
 
 pub const ALICE_PRIVATE_KEY: &str = include_str!("../assets/alice-key.pem");
-
 pub const ALICE_PRIVATE_KEY_DER: &[u8] = include_bytes!("../assets/alice-key.der");
 
 pub const ALICE_CERT: &str = include_str!("../assets/alice.pem");
-
 pub const ALICE_CERT_DER: &[u8] = include_bytes!("../assets/alice.der");
 
 pub const BOB_PRIVATE_KEY: &str = include_str!("../assets/bob-key.pem");
-
 pub const BOB_PRIVATE_KEY_DER: &[u8] = include_bytes!("../assets/bob-key.der");
 
 pub const BOB_CERT: &str = include_str!("../assets/bob.pem");
-
 pub const BOB_CERT_DER: &[u8] = include_bytes!("../assets/bob.der");
+
+pub const EVE_PRIVATE_KEY: &str = include_str!("../assets/eve-key.pem");
+pub const EVE_PRIVATE_KEY_DER: &[u8] = include_bytes!("../assets/eve-key.der");
+
+pub const EVE_CERT: &str = include_str!("../assets/eve.pem");
+pub const EVE_CERT_DER: &[u8] = include_bytes!("../assets/eve.der");
