1aee098158071519ef531ce13bc48ef520efc9ae
Maximilian Ammann <max@maxammann.org>
2022-07-07 19:51:07 +0200

    Fix panic during signing

--- tlspuffin/src/tls/fn_cert.rs
+++ tlspuffin/src/tls/fn_cert.rs
@@ -125,8 +125,21 @@ pub fn _fn_rsa_sign(
     private_key: &Vec<u8>,
     scheme: &SignatureScheme,
 ) -> Result<Vec<u8>, FnError> {
+    if match scheme {
+        SignatureScheme::RSA_PKCS1_SHA256 => true,
+        SignatureScheme::RSA_PKCS1_SHA384 => true,
+        SignatureScheme::RSA_PKCS1_SHA512 => true,
+        SignatureScheme::RSA_PSS_SHA256 => true,
+        SignatureScheme::RSA_PSS_SHA384 => true,
+        SignatureScheme::RSA_PSS_SHA512 => true,
+        _ => false,
+    } {
+        return Err(FnError::Rustls("Unknown signature scheme".to_string()));
+    }
+
     let key = RsaKeyPair::from_der(private_key)
         .map_err(|_| FnError::Rustls("Failed to parse rsa key.".to_string()))?;
+
     let signer = RsaSigner::new(Arc::new(key), *scheme);
     Ok(signer.sign(&message)?)
 }
