d5ac0c94bda7a9368ac8d4675e0c938c3152351d
HIRSCHI Lucca <lucca.hirschi@inria.fr>
2022-08-30 10:51:59 +0200

    oups

--- puffin/src/fuzzer/stats_monitor.rs
+++ puffin/src/fuzzer/stats_monitor.rs
@@ -399,9 +399,9 @@ where
     fn display(&mut self, event_msg: String, sender_id: u32) {
         self.log_count += 1;
 
-        // Only display log every 100 steps at the beginning and then every 1000 steps
-        if (self.log_count % 10000 != 0) ||
-            (self.log_count % 1000 != 0 && self.log_count < 1000000) ||
+        // Only display log every 100 steps at the beginning and then every 1000/10000 steps
+        if (self.log_count % 10000 != 0) &&
+            (self.log_count % 1000 != 0 && self.log_count < 1000000) &&
             (self.log_count % 100 != 0 && self.log_count < 10000) {
             return;
         }
