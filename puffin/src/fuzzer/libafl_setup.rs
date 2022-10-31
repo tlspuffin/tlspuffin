7242faa544dc3fb082a8d5ebbb8ce2107d2eb5a7
Maximilian Ammann <max@maxammann.org>
2022-10-31 16:32:30 +0100

    Set max_mutations_per_iteration to 4 and enable own mutator and stages

--- puffin/src/fuzzer/libafl_setup.rs
+++ puffin/src/fuzzer/libafl_setup.rs
@@ -86,7 +86,7 @@ impl Default for MutationStageConfig {
     fn default() -> Self {
         Self {
             max_iterations_per_stage: 256,
-            max_mutations_per_iteration: 16,
+            max_mutations_per_iteration: 4,
         }
     }
 }
@@ -258,11 +258,11 @@ where
             ..
         } = self.config;
 
-        // FIXME let mutator = PuffinScheduledMutator::new(self.mutations.unwrap(), max_mutations_per_iteration);
-        let mutator = StdScheduledMutator::new(self.mutations.unwrap());
+        let mutator =
+            PuffinScheduledMutator::new(self.mutations.unwrap(), max_mutations_per_iteration);
+        // let mutator = StdScheduledMutator::new(self.mutations.unwrap());
         let mut stages = tuple_list!(
-            // FIXMEPuffinMutationalStage::new(mutator, max_iterations_per_stage),
-            StdMutationalStage::new(mutator),
+            PuffinMutationalStage::new(mutator, max_iterations_per_stage),
             StatsStage::new()
         );
 
