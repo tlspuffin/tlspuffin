6f5dd9d7bb83ac928bdd6dac8a13e971856ddc21
Max Ammann <maximilian.ammann@trailofbits.com>
2023-04-18 21:22:14 +0200

    Add nix-shell for Linux and macOS

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.rustup
+  
+    pkgs.llvmPackages_14.llvm
+    pkgs.llvmPackages_14.clang
+
+    pkgs.cmake
+    # wolfSSL
+    pkgs.autoconf
+    pkgs.automake
+    pkgs.libtool
+
+    # macos
+    pkgs.libiconv
+
+    # openssh
+    pkgs.openssl_1_1
+
+    # Old openssl
+    pkgs.xorg.makedepend
+
+    pkgs.graphviz 
+    pkgs.yajl
+    pkgs.python310Packages.pip
+    pkgs.python310Packages.virtualenv
+  ];
+  shellHook = ''
+export LIBCLANG_PATH="${pkgs.llvmPackages_14.libclang.lib}/lib";
+  '';
+}
+
