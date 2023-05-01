c66d054f2019bca4c3d91f2a63206ebec09932af
Max Ammann <maximilian.ammann@trailofbits.com>
2023-05-01 20:02:00 +0200

    Update gitignore

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 count=0
-batch=500
+batch=100
+
+#corpus="/local-unsafe/mammann/2022-tlspuffin-evaluation-paper/evaluation-paper/parallel/experiments/2022-11-16-155256-21-SDOS1-0/corpus"
+corpus="/local-unsafe/mammann/2022-tlspuffin-evaluation-paper/evaluation-paper/parallel/experiments/2022-11-16-155256-21-SIG-0/corpus"
+
+features="wolfssl520"
+#features="openssl111j"
 
 gcovr --gcov-executable "llvm-cov gcov" -s -d > /dev/null 2>&1
 
 while true; do
-    time=$(cargo run -p tlspuffin --target x86_64-unknown-linux-gnu --features "openssl111j,gcov_analysis" --no-default-features  -- \
-        execute --index "$count" -n $batch /local-unsafe/mammann/2022-tlspuffin-evaluation-paper/evaluation-paper/parallel/experiments/2022-11-16-155256-21-SDOS1-0/corpus/*)
+    time=$(cargo run -p tlspuffin --target x86_64-unknown-linux-gnu --features "$features,gcov_analysis" --no-default-features  -- \
+        execute --index "$count" -n $batch \
+        $corpus/*)
+    
     exit_status=$?
     cov_data=$(gcovr --gcov-executable "llvm-cov gcov" -e ".*test.*" -e ".*apps.*" -e ".*crypt.*" -e ".*include.*" -e ".*engine.*" -j32 -s | grep "[lb][a-z]*:")
  
