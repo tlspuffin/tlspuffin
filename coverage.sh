4479b06411e3a6cdee673290b7bb1ff6886682b3
Max Ammann <maximilian.ammann@trailofbits.com>
2023-05-01 21:08:04 +0200

    Test coverage generation with wolfssl

--- coverage.sh
+++ coverage.sh
@@ -9,21 +9,25 @@ corpus="/local-unsafe/mammann/2022-tlspuffin-evaluation-paper/evaluation-paper/p
 features="wolfssl520"
 #features="openssl111j"
 
+#excludes=(-e ".*wolf.*" -e ".*test.*" -e ".*apps.*" -e ".*crypt.*" -e ".*include.*" -e ".*engine.*")
+excludes=(-e ".*openssl.*" -e ".*test.*" -e ".*wolfcrypt.*")
+
 gcovr --gcov-executable "llvm-cov gcov" -s -d > /dev/null 2>&1
 
 while true; do
     time=$(cargo run -p tlspuffin --target x86_64-unknown-linux-gnu --features "$features,gcov_analysis" --no-default-features  -- \
-        execute --index "$count" -n $batch \
-        $corpus/*)
+        execute --index "$count" -n $batch $corpus/*)
     
     exit_status=$?
-    cov_data=$(gcovr --gcov-executable "llvm-cov gcov" -e ".*test.*" -e ".*apps.*" -e ".*crypt.*" -e ".*include.*" -e ".*engine.*" -j32 -s | grep "[lb][a-z]*:")
+
+    cov_data=$(gcovr --gcov-executable "llvm-cov gcov" "${excludes[@]}" -j32 -s | grep "[lb][a-z]*:")
  
     l_per=$(echo "$cov_data" | grep lines | cut -d" " -f2 | rev | cut -c2- | rev)
     l_abs=$(echo "$cov_data" | grep lines | cut -d" " -f3 | cut -c2-)
     b_per=$(echo "$cov_data" | grep branch | cut -d" " -f2 | rev | cut -c2- | rev)
     b_abs=$(echo "$cov_data" | grep branch | cut -d" " -f3 | cut -c2-)
     
+    echo "$cov_data"
     echo "$time,$l_per,$l_abs,$b_per,$b_abs" >> coverage.csv
 
     count=$(expr $count + $batch)
