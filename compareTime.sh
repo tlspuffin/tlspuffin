cc1e3d6eecb2eedf88bf421a4c479d6090916560
Lucca Hirschi <lhirschi@cassis-calc.loria.fr>
2023-07-19 15:14:06 +0200

    Now computing # execs and # seconds

--- compareTime.sh
+++ compareTime.sh
@@ -9,10 +9,16 @@ find **/*ABLATION*/objective -not -empty -type d -prune | while read d; do
     date1=$(find "./${d}/../README.md" -printf "%TY-%Tm-%Td %TH:%TM:%TS\n")
     # ls -ldc $d
     date2=$(find "${d}/${df}" -printf "%TY-%Tm-%Td %TH:%TM:%TS\n")
-    echo "Comparing $date1 with $date2"
+#    echo "Comparing $date1 with $date2"
 
     date1=$(find "./${d}/../README.md" -printf "%TY%Tm%Td%TH%TM%TS\n")
     # ls -ldc $d
     date2=$(find "${d}/${df}" -printf "%TY%Tm%Td%TH%TM%TS\n")
-    dateutils.ddiff -i '%Y%m%d%H%M%S' $date1 $date2
+    diffDate=$(dateutils.ddiff -i '%Y%m%d%H%M%S' $date1 $date2 | cut -ds -f1)
+    echo -n "  Elapsed seconds: "
+    printf "%'.0f\n" $diffDate
+    
+    total_execs=$(tail -c 500 "${d}/../stats.json" | grep -E -o '"total_execs":.{0,12}' | cut -c15- | cut -d, -f1)
+    echo -n "  Total executions: "
+    printf "%'.0f\n" $total_execs
 done
