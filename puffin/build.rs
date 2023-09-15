8981c2e1992bb2f970edd07b4f4085d869c88f1d
Max Ammann <maximilian.ammann@trailofbits.com>
2023-09-15 11:34:56 +0100

    Fix crash if non-git directory

--- puffin/build.rs
+++ puffin/build.rs
@@ -1,10 +1,17 @@
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 
 pub fn get_git_ref() -> String {
     Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
         .ok()
+        .and_then(|output| {
+            if output.status.success() {
+                Some(output)
+            } else {
+                None
+            }
+        })
         .and_then(|output| String::from_utf8(output.stdout).ok())
         .map(|git_ref| git_ref[..6].to_string())
         .unwrap_or_else(|| "unknown".to_string())
