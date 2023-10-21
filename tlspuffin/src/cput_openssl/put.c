83bb760932eb83bfda138fa26593029bc88c14f7
Michael Mera <dev@michaelmera.com>
2023-10-21 12:36:34 +0200

    build(cput): fix C PUT build on linux
    C PUT would not build on linux because inttypes.h and stdio.h are not included
    in stdlib.h, unlike on MacOS.

--- tlspuffin/src/cput_openssl/put.c
+++ tlspuffin/src/cput_openssl/put.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <openssl/ssl.h>
 #include <openssl/decoder.h>
 
--- tlspuffin/src/cput_openssl/put.h
+++ tlspuffin/src/cput_openssl/put.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <inttypes.h>
 
 // FIXME: C/Rust duplication of type definitions
 //
