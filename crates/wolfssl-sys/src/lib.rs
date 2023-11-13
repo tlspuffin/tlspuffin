0834f410c9ebbece2c29b71cfed0301e325ccfed
Max Ammann <max@maxammann.org>
2023-11-13 18:19:56 +0100

    Fix certificates and CI (#287)
    * Regenerate certs and add generate script
    
    * Fix verbose log output
    
    * Allow warnings for wolfssl bindings
    
    * Remove dotnet to have more disk space available

--- crates/wolfssl-sys/src/lib.rs
+++ crates/wolfssl-sys/src/lib.rs
@@ -1,6 +1,3 @@
-#![allow(dead_code)]
-#![allow(non_camel_case_types)]
-#![allow(non_upper_case_globals)]
-#![allow(clippy)]
+#![allow(warnings)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
