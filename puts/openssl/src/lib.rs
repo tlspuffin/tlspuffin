a203d720f77223f690aa5ba386dd666971d7b41c
Michael Mera <dev@michaelmera.com>
2023-11-28 21:05:03 +0100

    build(cput_openssl): replace hardcoded OpenSSL dir with env variable

--- puts/openssl/src/lib.rs
+++ puts/openssl/src/lib.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-pub fn build() {
+pub fn build(ossl_libdir: String, ossl_incdir: String) {
     let out_dir = env::var("OUT_DIR").unwrap();
     let cbindings_include = env::var("DEP_TLSPUFFIN_CBINDINGS_INCLUDE").unwrap();
     let cput_source = concat!(env!("CARGO_MANIFEST_DIR"), "/src/put.c");
@@ -8,11 +8,12 @@ pub fn build() {
     println!("cargo:rerun-if-changed={}", cput_source);
 
     cc::Build::new()
+        .include(ossl_incdir)
         .include(cbindings_include)
         .file(cput_source)
         .compile("cputopenssl");
 
-    println!("cargo:rustc-link-search=native=openssl_build/openssl");
+    println!("cargo:rustc-link-search=native={}", ossl_libdir);
     println!("cargo:rustc-link-lib=static=ssl");
     println!("cargo:rustc-link-lib=static=crypto");
     println!("cargo:rustc-link-search=native={}", out_dir);
--- tlspuffin/build.rs
+++ tlspuffin/build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
@@ -26,5 +26,36 @@ fn main() {
     }
 
     #[cfg(feature = "cputopenssl")]
-    cputopenssl::build();
+    {
+        fn openssl_config() -> (PathBuf, PathBuf) {
+            if env::var("OSSL_DIR").is_ok() {
+                let ossl_dir = PathBuf::from(env::var("OSSL_DIR").unwrap());
+                let ossl_lib = ossl_dir.join("");
+                let ossl_inc = ossl_dir.join("include");
+
+                return (ossl_lib, ossl_inc);
+            }
+
+            if env::var("OSSL_VERSION").is_ok() {
+                // TODO build OpenSSL from sources
+                //
+                //     If no pre-built OpenSSL is provided through the OSSL_DIR
+                //     env variable then we build it from sources using the
+                //     provided OSSL_VERSION.
+                //
+                //     This variable should contain a valid reference to a git
+                //     branch or tag in the OpenSSL repository.
+                let _ = env::var("OSSL_VERSION").unwrap();
+            }
+
+            panic!("native dependency not found: need either OSSL_DIR or OSSL_VERSION environment variable");
+        }
+
+        let (libdir, incdir) = openssl_config();
+
+        cputopenssl::build(
+            libdir.to_string_lossy().into(),
+            incdir.to_string_lossy().into(),
+        );
+    }
 }
