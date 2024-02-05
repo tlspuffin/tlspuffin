1f65da5a0b92755b91ff492454d6b702fcca2476
Michael Mera <dev@michaelmera.com>
2024-02-05 13:55:01 +0100

    build: fix unknown lint directive `clippy`

--- crates/libssh-sys/src/lib.rs
+++ crates/libssh-sys/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
-#![allow(clippy)]
+#![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
