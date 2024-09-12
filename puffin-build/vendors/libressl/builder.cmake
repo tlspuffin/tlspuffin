346f2d67cd2b14f861f921b2aa28f601d8d7eeb5
Michael Mera <dev@michaelmera.com>
2024-09-12 09:17:13 +0200

    build: add a Rust implementation of `mk_vendor make`
    (cherry picked from commit af1a8241a9caff06bd99a7c6b80a66585b2d971d)

---
+++ puffin-build/vendors/libressl/builder.cmake
@@ -0,0 +1,42 @@
+use_languages(C)
+
+option(asan "Build with address-sanitizer" OFF)
+option(sancov "Build with sancov" OFF)
+option(gcov "Build with instrumentation for gcov coverage" OFF)
+option(llvm_cov "Build with instrumentation for llvm coverage" OFF)
+
+patch(PATTERN "s/USE_BUILTIN_ARC4RANDOM=no/USE_BUILTIN_ARC4RANDOM=yes/g" <SOURCE_DIR>/m4/check-os-options.m4)
+patch(PATTERN [===[s/\\$ac_cv_func_arc4random_buf/no/g]===] <SOURCE_DIR>/m4/check-libc.m4)
+
+list(APPEND PATCH_COMMANDS COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_LIST_DIR}/arc4random_prng.c" "<SOURCE_DIR>/crypto/compat/arc4random.c")
+list(APPEND PATCH_COMMANDS COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_LIST_DIR}/arc4random_prng.h" "<SOURCE_DIR>/crypto/compat/arc4random.h")
+
+autotools_builder(
+  FEATURES
+    --enable-static
+    --disable-shared
+    --disable-tests
+
+  CFLAGS
+    -g
+    -fPIC
+    -fvisibility=hidden
+    -I${CMAKE_SOURCE_DIR}/../../tlspuffin-claims
+
+    # SANCOV
+    $<$<BOOL:${sancov}>:-fsanitize-coverage=trace-pc-guard>
+
+    # ASAN
+    $<$<BOOL:${asan}>:-fsanitize=address>
+    $<$<BOOL:${asan}>:-shared-libsan>
+
+    # LLVM_COV
+    $<$<BOOL:${llvm_cov}>:-fprofile-instr-generate>
+    $<$<BOOL:${llvm_cov}>:-fcoverage-mapping>
+    $<$<BOOL:${llvm_cov}>:-O0>
+
+    # GCOV
+    $<$<BOOL:${gcov}>:-ftest-coverage>
+    $<$<BOOL:${gcov}>:-fprofile-arcs>
+    $<$<BOOL:${gcov}>:-O0>
+)
---
+++ puffin-build/vendors/libssh/builder.cmake
@@ -0,0 +1,44 @@
+use_languages(C)
+
+option(asan "Build with address-sanitizer" OFF)
+option(sancov "Build with sancov" OFF)
+option(gcov "Build with instrumentation for gcov coverage" OFF)
+option(llvm_cov "Build with instrumentation for llvm coverage" OFF)
+
+cmake_builder(
+  TARGETS
+    install
+
+  CMAKE_FLAGS
+    -DWITH_EXAMPLES=OFF
+    -DWITH_GSSAPI=OFF
+    -DWITH_SFTP=OFF
+    -DWITH_NACL=OFF
+    -DBUILD_SHARED_LIBS=OFF
+    -DCMAKE_POLICY_DEFAULT_CMP0148:STRING=OLD
+
+  CFLAGS
+    -g
+    -fPIC
+    -fvisibility=hidden
+    -I${CMAKE_SOURCE_DIR}/../../tlspuffin-claims
+    -Wno-error
+    -Wstrict-prototypes
+
+    # SANCOV
+    $<$<BOOL:${sancov}>:-fsanitize-coverage=trace-pc-guard>
+
+    # ASAN
+    $<$<BOOL:${asan}>:-fsanitize=address>
+    $<$<BOOL:${asan}>:-shared-libsan>
+
+    # LLVM_COV
+    $<$<BOOL:${llvm_cov}>:-fprofile-instr-generate>
+    $<$<BOOL:${llvm_cov}>:-fcoverage-mapping>
+    $<$<BOOL:${llvm_cov}>:-O0>
+
+    # GCOV
+    $<$<BOOL:${gcov}>:-ftest-coverage>
+    $<$<BOOL:${gcov}>:-fprofile-arcs>
+    $<$<BOOL:${gcov}>:-O0>
+)
