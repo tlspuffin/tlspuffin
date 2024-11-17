207ecfde6c81380b90b1c52e31c0aec547b896ae
Michael Mera <dev@michaelmera.com>
2024-11-17 15:41:35 +0100

    feat: implement C PUT for OpenSSL

--- tlspuffin/harness/openssl/src/put.c
+++ tlspuffin/harness/openssl/src/put.c
@@ -1,34 +1,417 @@
-#include <openssl/ssl.h>
-#include <puffin/tls.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define xstr(s) str(s)
-#define str(s) #s
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/opensslv.h>
+#include <openssl/rand.h>
+#include <openssl/ssl.h>
 
-struct AGENT
+#include <claim-interface.h>
+#include <puffin/tls.h>
+
+#include "bindings.h"
+#include "rng.h"
+
+extern const TLS_PUT_INTERFACE *REGISTER();
+
+struct AGENT_TYPE
 {
-    // TODO
+    uint8_t name;
+
+    SSL *ssl;
+
+    BIO *in;
+    BIO *out;
+
+    const CLAIMER_CB *claimer;
 };
 
+AGENT openssl_create(const TLS_AGENT_DESCRIPTOR *descriptor);
+AGENT openssl_create_client(const TLS_AGENT_DESCRIPTOR *descriptor);
+AGENT openssl_create_server(const TLS_AGENT_DESCRIPTOR *descriptor);
+void openssl_destroy(AGENT agent);
+RESULT openssl_progress(AGENT agent);
+RESULT openssl_reset(AGENT agent, uint8_t new_name);
+bool openssl_is_successful(AGENT agent);
+const char *openssl_describe_state(AGENT agent);
+bool openssl_is_successful(AGENT agent);
+RESULT openssl_add_inbound(AGENT agent, const uint8_t *bytes, size_t length, size_t *written);
+RESULT openssl_take_outbound(AGENT agent, uint8_t *bytes, size_t max_length, size_t *readbytes);
+void openssl_register_claimer(AGENT agent, const CLAIMER_CB *claimer);
+
+static RESULT_CODE result_code(AGENT agent, int retcode);
+
+static AGENT make_agent(SSL_CTX *ssl_ctx, const TLS_AGENT_DESCRIPTOR *descriptor);
+
+static void default_claimer_notify(void *context, Claim *claim)
+{
+    _log(PUFFIN.trace, "call to default claimer `notify`");
+};
+
+static void default_claimer_destroy(void *context)
+{
+    _log(PUFFIN.trace, "call to default claimer `destroy`");
+};
+
+static const CLAIMER_CB DEFAULT_CLAIMER_CB = {.context = NULL,
+                                              .notify = default_claimer_notify,
+                                              .destroy = default_claimer_destroy};
+
+// static C_PUT_INTERFACE OPENSSL_PUT = {
+//     .create = openssl_create,
+//     .destroy = openssl_destroy,
+
+//     .rng_reseed = rng_reseed,
+
+//     .progress = openssl_progress,
+//     .reset = openssl_reset,
+//     .describe_state = openssl_describe_state,
+//     .is_state_successful = openssl_is_successful,
+
+//     .register_claimer = openssl_register_claimer,
+
+//     .add_inbound = openssl_add_inbound,
+//     .take_outbound = openssl_take_outbound,
+// };
+
 static const TLS_PUT_INTERFACE OPENSSL_PUT = {
-    .create = NULL,
+    .create = openssl_create,
     .rng_reseed = NULL,
     .supports = NULL,
 
     .agent_interface =
         {
-            .destroy = NULL,
-            .progress = NULL,
-            .reset = NULL,
-            .describe_state = NULL,
-            .is_state_successful = NULL,
+            .destroy = openssl_destroy,
+            .progress = openssl_progress,
+            .reset = openssl_reset,
+            .describe_state = openssl_describe_state,
+            .is_state_successful = openssl_is_successful,
+            .register_claimer = openssl_register_claimer,
 
-            .add_inbound = NULL,
-            .take_outbound = NULL,
+            .add_inbound = openssl_add_inbound,
+            .take_outbound = openssl_take_outbound,
         },
 };
 
 const TLS_PUT_INTERFACE *REGISTER()
 {
+    openssl_init();
+
     return &OPENSSL_PUT;
+}
+
+const int tls_version[] = {TLS1_3_VERSION, TLS1_2_VERSION};
+const char *version_str[] = {"V1_3", "V1_2"};
+const char *type_str[] = {"client", "server"};
+
+AGENT openssl_create(const TLS_AGENT_DESCRIPTOR *descriptor)
+{
+    _log(PUFFIN.info,
+         "descriptor %u version: %s type: %s",
+         descriptor->name,
+         version_str[descriptor->tls_version],
+         type_str[descriptor->role]);
+
+    if (tls_version[descriptor->tls_version] == TLS_UNSUPPORTED_VERSION)
+    {
+        _log(PUFFIN.error, "unsupported TLS version: %s", version_str[descriptor->tls_version]);
+        return NULL;
+    }
+
+    if (descriptor->role == CLIENT)
+    {
+        return openssl_create_client(descriptor);
+    }
+
+    if (descriptor->role == SERVER)
+    {
+        return openssl_create_server(descriptor);
+    }
+
+    _log(PUFFIN.error,
+         "unknown agent type for descriptor %u: %u",
+         descriptor->name,
+         descriptor->role);
+    return NULL;
+}
+
+void openssl_destroy(AGENT agent)
+{
+    if (agent->claimer != NULL)
+    {
+        agent->claimer->destroy(agent->claimer->context);
+    }
+
+    SSL_free(agent->ssl);
+    free(agent);
+}
+
+RESULT openssl_progress(AGENT agent)
+{
+    if (!openssl_is_successful(agent))
+    {
+        // not connected yet -> do handshake
+        int ret = SSL_do_handshake(agent->ssl);
+
+        RESULT_CODE ecode = result_code(agent, ret);
+        if (ecode == RESULT_IO_WOULD_BLOCK)
+        {
+            ecode = RESULT_OK;
+        }
+
+        char *reason = get_error_reason();
+        RESULT result = PUFFIN.make_result(ecode, reason);
+        free(reason);
+
+        return result;
+    }
+
+    // trigger another read
+    uint8_t buf[128];
+    int ret = SSL_read(agent->ssl, &buf, 128);
+    if (ret > 0)
+    {
+        return PUFFIN.make_result(RESULT_OK, NULL);
+    }
+
+    RESULT_CODE ecode = result_code(agent, ret);
+    if (ecode == RESULT_IO_WOULD_BLOCK)
+    {
+        ecode = RESULT_OK;
+    }
+
+    char *reason = get_error_reason();
+    RESULT result = PUFFIN.make_result(ecode, reason);
+    free(reason);
+
+    return result;
+}
+
+RESULT openssl_reset(AGENT agent, uint8_t new_name)
+{
+    agent->name = new_name;
+
+    openssl_register_claimer(agent, &DEFAULT_CLAIMER_CB);
+
+    int ret = SSL_clear(agent->ssl);
+    if (ret == 0)
+    {
+        return PUFFIN.make_result(RESULT_ERROR_OTHER, get_error_reason());
+    }
+
+    return PUFFIN.make_result(RESULT_OK, NULL);
+}
+
+const char *openssl_describe_state(AGENT agent)
+{
+    // NOTE: Very useful for nonblocking according to docs:
+    //     https://www.openssl.org/docs/manmaster/man3/SSL_state_string.html
+    //
+    //     When using nonblocking sockets, the function call performing the
+    //     handshake may return with SSL_ERROR_WANT_READ or SSL_ERROR_WANT_WRITE
+    //     condition, so that SSL_state_string[_long]() may be called.
+    return SSL_state_string_long(agent->ssl);
+}
+
+bool openssl_is_successful(AGENT agent)
+{
+    return (strstr(openssl_describe_state(agent), "SSL negotiation finished successfully") != NULL);
+}
+
+#ifdef HAS_CLAIMS
+void _inner_claimer(Claim claim, void *ctx)
+{
+    AGENT agent = (AGENT)(ctx);
+    agent->claimer->notify(agent->claimer->context, &claim);
+}
+#endif
+
+void openssl_register_claimer(AGENT agent, const CLAIMER_CB *claimer)
+{
+    if (agent->claimer != NULL)
+    {
+        agent->claimer->destroy(agent->claimer->context);
+    }
+
+    CLAIMER_CB *new_claimer = malloc(sizeof(CLAIMER_CB));
+    memcpy(new_claimer, claimer, sizeof(CLAIMER_CB));
+    agent->claimer = new_claimer;
+
+#ifdef HAS_CLAIMS
+    register_claimer(agent->ssl, _inner_claimer, agent);
+#endif
+}
+
+RESULT openssl_add_inbound(AGENT agent, const uint8_t *bytes, size_t length, size_t *written)
+{
+    int ret = BIO_write_ex(agent->in, bytes, length, written);
+
+    RESULT_CODE ecode = result_code(agent, ret);
+    char *reason = get_error_reason();
+    RESULT result = PUFFIN.make_result(ecode, reason);
+    free(reason);
+
+    return result;
+}
+
+RESULT openssl_take_outbound(AGENT agent, uint8_t *bytes, size_t max_length, size_t *readbytes)
+{
+    int ret = BIO_read_ex(agent->out, bytes, max_length, readbytes);
+
+    RESULT_CODE ecode = result_code(agent, ret);
+    char *reason = get_error_reason();
+    RESULT result = PUFFIN.make_result(ecode, reason);
+    free(reason);
+
+    return result;
+}
+
+AGENT openssl_create_client(const TLS_AGENT_DESCRIPTOR *descriptor)
+{
+    SSL_CTX *ssl_ctx = SSL_CTX_new(TLS_method());
+
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L
+    // Not sure whether we want this disabled or enabled:
+    // https://github.com/tlspuffin/tlspuffin/issues/67 The tests become simpler if disabled to
+    // maybe that's what we want. Lets leave it default for now.
+    // https://wiki.openssl.org/index.php/TLS1.3#Middlebox_Compatibility_Mode
+    SSL_CTX_clear_options(ssl_ctx, SSL_OP_ENABLE_MIDDLEBOX_COMPAT);
+#endif
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    SSL_CTX_set_max_proto_version(ssl_ctx, tls_version[descriptor->tls_version]);
+#endif
+
+    // Disallow EXPORT in client
+    SSL_CTX_set_cipher_list(ssl_ctx, "ALL:!EXPORT:!LOW:!aNULL:!eNULL:!SSLv2");
+    SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_NONE, NULL);
+
+    if (descriptor->client_authentication)
+    {
+        ssl_ctx = set_cert(ssl_ctx, descriptor->cert);
+        ssl_ctx = set_pkey(ssl_ctx, descriptor->pkey);
+        if (ssl_ctx == NULL)
+        {
+            return NULL;
+        }
+    }
+
+    if (descriptor->server_authentication)
+    {
+        SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+
+        ssl_ctx = set_store(ssl_ctx, descriptor->store, descriptor->store_length);
+        if (ssl_ctx == NULL)
+        {
+            return NULL;
+        }
+    }
+
+    AGENT agent = make_agent(ssl_ctx, descriptor);
+    if (agent == NULL)
+    {
+        return NULL;
+    }
+
+    SSL_set_connect_state(agent->ssl);
+
+    return agent;
+}
+
+AGENT openssl_create_server(const TLS_AGENT_DESCRIPTOR *descriptor)
+{
+    SSL_CTX *ssl_ctx = SSL_CTX_new(TLS_method());
+
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L
+    SSL_CTX_set_options(ssl_ctx, SSL_OP_ALLOW_NO_DHE_KEX);
+    SSL_CTX_clear_options(ssl_ctx, SSL_OP_ENABLE_MIDDLEBOX_COMPAT);
+#endif
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    SSL_CTX_set_max_proto_version(ssl_ctx, tls_version[descriptor->tls_version]);
+#endif
+
+#if OPENSSL_VERSION_NUMBER > 0x10002000L
+    SSL_CTX_set_ecdh_auto(ssl_ctx, 1);
+#else
+    EC_KEY *ecdh = EC_KEY_new_by_curve_name(NID_secp384r1);
+    SSL_CTX_set_tmp_ecdh(ssl_ctx, ecdh);
+#endif
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+    RSA *rsa = RSA_generate_key(512, RSA_F4, NULL, NULL);
+    SSL_CTX_set_tmp_rsa(ssl_ctx, rsa);
+#endif
+
+    // Allow EXPORT in server
+    SSL_CTX_set_cipher_list(ssl_ctx, "ALL:EXPORT:!LOW:!aNULL:!eNULL:!SSLv2");
+    SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_NONE, NULL);
+
+    ssl_ctx = set_cert(ssl_ctx, descriptor->cert);
+    ssl_ctx = set_pkey(ssl_ctx, descriptor->pkey);
+    if (ssl_ctx == NULL)
+    {
+        return NULL;
+    }
+
+    if (descriptor->client_authentication)
+    {
+        SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+
+        ssl_ctx = set_store(ssl_ctx, descriptor->store, descriptor->store_length);
+        if (ssl_ctx == NULL)
+        {
+            return NULL;
+        }
+    }
+
+    AGENT agent = make_agent(ssl_ctx, descriptor);
+    if (agent == NULL)
+    {
+        return NULL;
+    }
+
+    SSL_set_accept_state(agent->ssl);
+
+    return agent;
+}
+
+static AGENT make_agent(SSL_CTX *ssl_ctx, const TLS_AGENT_DESCRIPTOR *descriptor)
+{
+    SSL *ssl = SSL_new(ssl_ctx);
+
+    AGENT agent = malloc(sizeof(struct AGENT_TYPE));
+    agent->name = descriptor->name;
+    agent->ssl = ssl;
+    agent->in = BIO_new(BIO_s_mem());
+    agent->out = BIO_new(BIO_s_mem());
+
+    agent->claimer = &DEFAULT_CLAIMER_CB;
+    openssl_register_claimer(agent, &DEFAULT_CLAIMER_CB);
+
+    SSL_set_bio(agent->ssl, agent->in, agent->out);
+    SSL_CTX_free(ssl_ctx);
+
+    return agent;
+}
+
+static RESULT_CODE result_code(AGENT agent, int retcode)
+{
+    int ssl_ecode = SSL_get_error(agent->ssl, retcode);
+    switch (ssl_ecode)
+    {
+    case SSL_ERROR_NONE:
+        return RESULT_OK;
+
+    case SSL_ERROR_WANT_READ:
+    case SSL_ERROR_WANT_WRITE:
+        return RESULT_IO_WOULD_BLOCK;
+    default:
+        return RESULT_ERROR_OTHER;
+    }
 }
--- tlspuffin/src/put.rs
+++ tlspuffin/src/put.rs
@@ -1,18 +1,26 @@
 use std::cell::RefCell;
 use std::collections::HashSet;
+use std::io::Read;
 use std::rc::Rc;
 
 use puffin::agent::{AgentDescriptor, AgentType};
 use puffin::algebra::dynamic_function::TypeShape;
 use puffin::claims::GlobalClaimList;
 use puffin::error::Error;
-use puffin::protocol::ProtocolBehavior;
+use puffin::harness::{to_string, CError};
+use puffin::protocol::{OpaqueProtocolMessageFlight, ProtocolBehavior, ProtocolMessageDeframer};
 use puffin::put::{Put, PutOptions};
 use puffin::put_registry::Factory;
+use puffin::stream::Stream;
+use security_claims::Claim;
 
 use crate::claims::TlsClaim;
-use crate::protocol::{TLSProtocolBehavior, TLSProtocolTypes};
-use crate::put_registry::bindings::TLS_PUT_INTERFACE;
+use crate::protocol::{OpaqueMessageFlight, TLSProtocolBehavior, TLSProtocolTypes};
+use crate::put_registry::bindings::{
+    AGENT, CLAIMER_CB, PEM, TLS_AGENT_DESCRIPTOR, TLS_AGENT_ROLE, TLS_PUT_INTERFACE, TLS_VERSION,
+};
+use crate::static_certs::{ALICE_CERT, ALICE_PRIVATE_KEY, BOB_CERT, BOB_PRIVATE_KEY, EVE_CERT};
+use crate::tls::rustls::msgs::deframer::MessageDeframer;
 
 /// Static configuration for creating a new agent state for the PUT
 #[derive(Clone, Debug)]
@@ -78,11 +86,14 @@ impl CPut {
 impl Factory<TLSProtocolBehavior> for CPut {
     fn create(
         &self,
-        _agent_descriptor: &AgentDescriptor,
-        _claims: &GlobalClaimList<<TLSProtocolBehavior as ProtocolBehavior>::Claim>,
-        _options: &PutOptions,
+        agent_descriptor: &AgentDescriptor,
+        claims: &GlobalClaimList<<TLSProtocolBehavior as ProtocolBehavior>::Claim>,
+        options: &PutOptions,
     ) -> Result<Box<dyn Put<TLSProtocolBehavior>>, Error> {
-        todo!()
+        Ok(Box::new(CAgent::new(
+            self,
+            TlsPutConfig::new(agent_descriptor, claims, options),
+        )?))
     }
 
     fn name(&self) -> String {
@@ -116,5 +127,288 @@ impl Factory<TLSProtocolBehavior> for CPut {
 
     fn clone_factory(&self) -> Box<dyn Factory<TLSProtocolBehavior>> {
         Box::new(self.clone())
+    }
+}
+
+pub struct CAgent {
+    put: CPut,
+    config: TlsPutConfig,
+    deframer: MessageDeframer,
+    c_agent: AGENT,
+}
+
+macro_rules! pem {
+    ($pemder:expr) => {
+        PEM {
+            bytes: $pemder.0.as_ptr(),
+            length: $pemder.0.len(),
+        }
+    };
+}
+
+macro_rules! ccall {
+    ( $put:expr, $function_name:ident ) => {
+        ($put.interface.agent_interface.$function_name.unwrap())()
+    };
+    ( $put:expr, $function_name:ident, $($arg:expr),*) => {
+        ($put.interface.agent_interface.$function_name.unwrap())($($arg),*)
+    };
+}
+
+macro_rules! take_res {
+    ($call:expr) => {
+        *unsafe { Box::from_raw($call as *mut Result<String, CError>) }
+    };
+}
+
+macro_rules! r_ccall {
+    ( $put:expr, $function_name:ident ) => {
+        take_res!(ccall!($put, $function_name))
+    };
+    ( $put:expr, $function_name:ident, $($arg:expr),*) => {
+        take_res!(ccall!($put, $function_name, $($arg),*))
+    };
+}
+
+impl CAgent {
+    fn new(put: &CPut, config: TlsPutConfig) -> Result<Self, Error> {
+        let descriptor = match config.descriptor.typ {
+            AgentType::Server => make_descriptor(
+                &config,
+                &pem!(ALICE_CERT),
+                &pem!(ALICE_PRIVATE_KEY),
+                &[&pem!(BOB_CERT) as *const _, &pem!(EVE_CERT) as *const _],
+            ),
+            AgentType::Client => make_descriptor(
+                &config,
+                &pem!(BOB_CERT),
+                &pem!(BOB_PRIVATE_KEY),
+                &[&pem!(ALICE_CERT) as *const _, &pem!(EVE_CERT) as *const _],
+            ),
+        };
+
+        let c_agent = unsafe { (put.interface.create.unwrap())(&descriptor as *const _) };
+        if c_agent.is_null() {
+            return Err(Error::Put("C agent creation failed".to_owned()));
+        }
+
+        let mut agent = Self {
+            put: put.clone(),
+            config,
+            deframer: MessageDeframer::new(),
+            c_agent,
+        };
+
+        agent.register_claimer();
+
+        Ok(agent)
+    }
+
+    fn register_claimer(&mut self) {
+        unsafe {
+            use crate::claims::claims_helpers;
+
+            let claims = self.config.claims.clone();
+            let protocol_version = self.config.descriptor.tls_version;
+            let origin = self.config.descriptor.typ;
+            let agent_name = self.config.descriptor.name;
+
+            let claimer = make_claimer(move |claim: Claim| {
+                if let Some(data) = claims_helpers::to_claim_data(protocol_version, claim) {
+                    claims.deref_borrow_mut().claim_sized(TlsClaim {
+                        agent_name,
+                        origin,
+                        protocol_version,
+                        data,
+                    })
+                }
+            });
+
+            ccall!(
+                self.put,
+                register_claimer,
+                self.c_agent,
+                &claimer as *const _
+            );
+        }
+    }
+}
+
+impl Put<TLSProtocolBehavior> for CAgent {
+    fn progress(&mut self) -> Result<(), Error> {
+        r_ccall!(self.put, progress, self.c_agent)?;
+        Ok(())
+    }
+
+    fn reset(&mut self, new_name: puffin::agent::AgentName) -> Result<(), Error> {
+        self.config.descriptor.name = new_name;
+        r_ccall!(self.put, reset, self.c_agent, new_name.into())?;
+        self.register_claimer();
+        Ok(())
+    }
+
+    fn descriptor(&self) -> &AgentDescriptor {
+        &self.config.descriptor
+    }
+
+    fn describe_state(&self) -> String {
+        unsafe { to_string(ccall!(self.put, describe_state, self.c_agent)) }
+    }
+
+    fn is_state_successful(&self) -> bool {
+        unsafe { ccall!(self.put, is_state_successful, self.c_agent) }
+    }
+
+    fn shutdown(&mut self) -> String {
+        todo!()
+    }
+
+    fn version() -> String
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
+
+impl Stream<TLSProtocolBehavior> for CAgent {
+    fn add_to_inbound(&mut self, message: &puffin::algebra::ConcreteMessage) {
+        let mut written = 0usize;
+        let result = r_ccall!(
+            self.put,
+            add_inbound,
+            self.c_agent,
+            message.as_ptr(),
+            message.len(),
+            &mut written as *mut usize
+        );
+
+        if let Err(cerror) = result {
+            log::error!("C PUT agent add_to_inbound() failed: {}", cerror.reason);
+        }
+    }
+
+    fn take_message_from_outbound(
+        &mut self,
+    ) -> Result<Option<<TLSProtocolBehavior as ProtocolBehavior>::OpaqueProtocolMessageFlight>, Error>
+    {
+        let mut flight = OpaqueMessageFlight::new();
+        loop {
+            if let Some(opaque_message) = self.deframer.pop_frame() {
+                flight.push(opaque_message);
+            } else {
+                let mut reader = CReader {
+                    put: &self.put,
+                    c_agent: self.c_agent,
+                };
+
+                match self.deframer.read(&mut reader) {
+                    Ok(v) => {
+                        if v == 0 {
+                            break;
+                        }
+                    }
+                    Err(err) => match err.kind() {
+                        std::io::ErrorKind::WouldBlock => {
+                            // This is not a hard error. It just means we will should read again
+                            // from the TCPStream in the next steps.
+                            break;
+                        }
+                        _ => return Err(err.into()),
+                    },
+                }
+            }
+        }
+
+        Ok((!flight.messages.is_empty()).then_some(flight))
+    }
+}
+
+impl Drop for CAgent {
+    fn drop(&mut self) {
+        unsafe {
+            ccall!(self.put, destroy, self.c_agent);
+        }
+    }
+}
+
+fn make_descriptor(
+    config: &TlsPutConfig,
+    cert: &PEM,
+    pkey: &PEM,
+    store: &[*const PEM],
+) -> TLS_AGENT_DESCRIPTOR {
+    TLS_AGENT_DESCRIPTOR {
+        name: config.descriptor.name.into(),
+        role: match config.descriptor.typ {
+            AgentType::Client => TLS_AGENT_ROLE::CLIENT,
+            AgentType::Server => TLS_AGENT_ROLE::SERVER,
+        },
+        tls_version: match config.descriptor.tls_version {
+            puffin::agent::TLSVersion::V1_3 => TLS_VERSION::V1_3,
+            puffin::agent::TLSVersion::V1_2 => TLS_VERSION::V1_2,
+        },
+        client_authentication: config.descriptor.client_authentication,
+        server_authentication: config.descriptor.server_authentication,
+
+        cert: cert as *const _,
+        pkey: pkey as *const _,
+
+        store: store.as_ptr(),
+        store_length: store.len() as libc::size_t,
+    }
+}
+
+extern "C" fn notify(ctx: *mut libc::c_void, c: *mut Claim) {
+    if c.is_null() || ctx.is_null() {
+        return;
+    }
+
+    let c = unsafe { *c };
+    let callback: &mut Box<dyn FnMut(Claim)> =
+        unsafe { &mut *(ctx as *mut std::boxed::Box<dyn FnMut(Claim)>) };
+
+    callback(c);
+}
+
+extern "C" fn destroy(ctx: *mut libc::c_void) {
+    let _: Box<Box<dyn FnMut(Claim)>> = unsafe { Box::from_raw(ctx as *mut _) };
+}
+
+fn make_claimer<F>(callback: F) -> CLAIMER_CB
+where
+    F: FnMut(Claim) + 'static,
+{
+    let cb: Box<Box<dyn FnMut(Claim)>> = Box::new(Box::new(callback));
+
+    CLAIMER_CB {
+        context: Box::into_raw(cb) as *mut _,
+        notify: Some(notify),
+        destroy: Some(destroy),
+    }
+}
+
+struct CReader<'a> {
+    put: &'a CPut,
+    c_agent: AGENT,
+}
+
+impl<'a> Read for CReader<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let mut readbytes = 0usize as libc::size_t;
+
+        let result = r_ccall!(
+            self.put,
+            take_outbound,
+            self.c_agent,
+            buf.as_mut_ptr(),
+            buf.len(),
+            &mut readbytes
+        );
+
+        match result {
+            Ok(_) => Ok(readbytes),
+            Err(cerror) => Err(cerror.into()),
+        }
     }
 }
