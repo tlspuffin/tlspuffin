41b6a6cd802e3cb7f03a9fae042ed66f851073a8
HIRSCHI Lucca <lucca.hirschi@inria.fr>
2025-03-13 15:28:05 +0100

    WIP: add 1 symbol

--- opcuapuffin/src/opcua/mod.rs
+++ opcuapuffin/src/opcua/mod.rs
@@ -4,10 +4,19 @@
 //! [`DynamicFunction`](puffin::algebra::dynamic_function::DynamicFunction)s which can be used in
 //! the fuzzing.
 
+use puffin::protocol::EvaluatedTerm;
+use puffin::codec;
+use puffin::trace::Source;
+use puffin::protocol::ProtocolTypes;
+use puffin::trace::Knowledge;
+use puffin::protocol::Extractable;
+use puffin::dummy_codec;
+use puffin::dummy_extract_knowledge;
+use puffin::dummy_extract_knowledge_codec;
 use puffin::algebra::dynamic_function::FunctionAttributes;
-//use puffin::algebra::error::FnError;
+use puffin::algebra::error::FnError;
 use puffin::define_signature; //puffin/algebra/signature.rs
-//use puffin::error::Error;
+use puffin::error::Error;
 
 use crate::protocol::OpcuaProtocolTypes;
 
@@ -18,19 +27,28 @@ pub mod seeds;
 pub mod fn_constants;
 pub use fn_constants::*;
 
-// pub struct MSG_type {
-//     payload: Vec<u8>,
-// }
+#[derive(Debug,Clone)]
+pub struct OPN_channel_request {
+    payload: Vec<u8>,
+}
 
-// pub fn fn_open_channel_request() -> Result<MSG_type, FnError> {
-//     Ok(MSG)
-// }
+dummy_extract_knowledge_codec!(
+    OpcuaProtocolTypes,
+    OPN_channel_request
+);
+
+pub fn fn_open_channel_request() -> Result<OPN_channel_request, FnError> {
+    Ok(OPN_channel_request {
+        payload: vec![0x01, 0x02, 0x03, 0x04],
+    })
+}
 
 define_signature!{
     OPCUA_SIGNATURE<OpcuaProtocolTypes>,
     // constants
     // fn_true
     // fn_false
-    fn_seq_0
+    // fn_seq_0
+    fn_open_channel_request
     //fn_open_channel_request
 }
