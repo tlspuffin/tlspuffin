90a0006c81e0622827484c388115915515ddf604
Olivier <olivier.demengeon@inria.fr>
2025-03-20 16:06:46 +0100

    wolfSSL cput:   Add function to report time to wolfssl modified build system:   Add flags in wolfSSL build to use time functions provided outside wolfSSL

--- puffin-build/vendors/wolfssl/builder.cmake
+++ puffin-build/vendors/wolfssl/builder.cmake
@@ -54,6 +54,7 @@ autotools_builder(
     --enable-psk # FIXME only 4.3.0
     --disable-examples
     --enable-cryptocb
+    --disable-crypttests # to be able to build with -DUSER_TICKS
 
     $<$<VERSION_GREATER_EQUAL:${VENDOR_VERSION},5.0.0>:--enable-context-extra-user-data>
     $<$<VERSION_GREATER_EQUAL:${VENDOR_VERSION},5.0.0>:--enable-dtls-mtu>
@@ -73,6 +74,7 @@ autotools_builder(
     -DHAVE_EX_DATA                # FIXME only 4.3.0
     -DWOLFSSL_CALLBACKS           # FIXME else some msg callbacks are not called
     -DHAVE_CURVE25519
+    -DUSER_TICKS                  # FIXME to ensure deterministic behaviour
     # FIXME broken: -DHAVE_EX_DATA_CLEANUP_HOOKS  # required for cleanup of ex data
     # FIXME broken: -DWC_RNG_SEED_CB              # makes test test_seed_cve_2022_38153 fail, but should be used when evaluating coverage to get same coverage than other fuzzers which use this flag to disable determinism
     # FIXME broken: -DWOLFSSL_GENSEED_FORTEST     # makes test test_seed_cve_2022_38153 fail, but should be used when evaluating coverage to get same coverage than other fuzzers which use this flag to disable determinism
