e64352cd9d6694022a6649a8fad86fc1d02ed930
Vincent DIEMUNSCH <vincent.diemunsch@inria.fr>
2025-03-26 10:23:40 +0100

    Rebased current branch on main to get the macros for Extractable (PR 376)

--- opcuapuffin/src/opcua/rustopcua/mod.rs
+++ opcuapuffin/src/opcua/rustopcua/mod.rs
@@ -3,7 +3,7 @@
 //! version = "0.13.0" # OPCUARustVersion
 //! authors = ["Adam Lock <locka99@gmail.com>"]
 //! license = "MPL-2.0"
-//! documentation = "https://docs.rs/opcua/"
+//! documentation = "<https://docs.rs/opcua/>"
 //!
 //! See [opcua](https://github.com/locka99/opcua/commit/fcc89d8f8b93b5a0943ec8086706e883900faa3c) fork.
 //! Upstreaming this fork is becoming unfeasible and there are no benefits in keeping up with the
