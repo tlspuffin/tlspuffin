aeaf8cc64bb8ccdae048af0839d6858aa70ae17d
HIRSCHI Lucca <lucca.hirschi@inria.fr>
2025-04-07 15:57:28 +0200

    fuzzing: add [no_bit] flags in signature, preventing MakeMessage and bit-level mutations to apply. Use it on very large encodings to avoid slowing down the fuzzer. This allows to resurrect `fn_large_bytes_vec` and the test tcp_wolfssl_openssl_test_seed_cve_2022_38153.

--- puffin/src/algebra/signature.rs
+++ puffin/src/algebra/signature.rs
@@ -155,6 +155,7 @@ macro_rules! define_signature {
                                     "list" => attrs.is_list = true,
                                     "get" => attrs.is_get = true,
                                     "no_gen" => attrs.no_gen = true,
+                                    "no_bit" => attrs.no_bit = true,
                                     _ => {},
                                 }
                             )*
