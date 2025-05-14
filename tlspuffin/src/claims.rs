6feea0d96d6412515bbfc3de11e77ff199829c7c
Olivier <olivier.demengeon@inria.fr>
2025-05-14 12:30:18 +0200

    CI tests

--- tlspuffin/src/claims.rs
+++ tlspuffin/src/claims.rs
@@ -375,7 +375,7 @@ pub mod claims_helpers {
             // Transcripts in these messages are not up-to-date. They get updated after the Message
             // has been processed
             security_claims::ClaimType::CLAIM_FINISHED => {
-                let peer_certificate = if claim.peer_authentication == 1
+                /*let peer_certificate = if claim.peer_authentication == 1
                     && matches!(claim.peer_cert.data_length, 1..1024)
                 {
                     SmallVec::from_slice(
@@ -383,7 +383,8 @@ pub mod claims_helpers {
                     )
                 } else {
                     SmallVec::new()
-                };
+                };*/
+                let peer_certificate0 = SmallVec::new();
                 Some(ClaimData::Message(ClaimDataMessage::Finished(Finished {
                     outbound: claim.write > 0,
                     client_random: SmallVec::from(claim.client_random.data),
@@ -391,8 +392,8 @@ pub mod claims_helpers {
                     session_id: SmallVec::from_slice(
                         &claim.session_id.data[..claim.session_id.length as usize],
                     ),
-                    authenticate_peer: claim.peer_authentication == 1,
-                    peer_certificate,
+                    authenticate_peer: false/*claim.peer_authentication == 1*/,
+                    peer_certificate: peer_certificate0,
                     master_secret: match protocol_version {
                         TLSVersion::V1_3 => SmallVec::from_slice(&claim.master_secret.secret),
                         TLSVersion::V1_2 => SmallVec::from_slice(&claim.master_secret_12.secret),
