d6999f9cfcfc3db3ed99a462fe1a56ac657cccce
Tom Gouville <tom.gouville@protonmail.com>
2025-06-16 14:41:31 +0200

    [tlspuffin] Add early secret and handshake secret to Finished claim

--- tlspuffin/src/rust_put/wolfssl/mod.rs
+++ tlspuffin/src/rust_put/wolfssl/mod.rs
@@ -426,6 +426,8 @@ impl RustPut {
                                 available_ciphers: Default::default(), // TODO
                                 signature_algorithm: 0,                // TODO
                                 peer_signature_algorithm: 0,           // TODO
+                                early_secret: Default::default(),
+                                handshake_secret: Default::default(),
                             })),
                             step: None,
                         });
