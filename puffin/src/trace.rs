3e6ff54ec8b28c5650f1d848422a3521d4b8156f
Tom Gouville <tom.gouville@protonmail.com>
2025-06-26 15:49:35 +0200

    [differential fuzzing] add security claims violation as differences

--- puffin/src/trace.rs
+++ puffin/src/trace.rs
@@ -577,7 +577,7 @@ impl<PB: ProtocolBehavior> TraceContext<PB> {
         &self,
         other: &Self,
         descriptors: &Vec<AgentDescriptor<<PB::ProtocolTypes as ProtocolTypes>::PUTConfig>>,
-    ) -> Result<(), Vec<TraceDifference>> {
+    ) -> Vec<TraceDifference> {
         let mut res = vec![];
 
         // Decrypting knowledges
@@ -630,11 +630,7 @@ impl<PB: ProtocolBehavior> TraceContext<PB> {
         // Comparing the computed terms
         res.extend(self_store.compare(&other_store).err().map_or(vec![], |x| x));
 
-        if res.is_empty() {
-            Ok(())
-        } else {
-            Err(res)
-        }
+        res
     }
 }
 
