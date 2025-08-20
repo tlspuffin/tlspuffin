4424ec490e645379ff54e04ab0cf5a2f4c2008fd
HIRSCHI Lucca <lucca.hirschi@inria.fr>
2025-08-20 17:03:07 +0200

    fuzzer: add bit-level mutations and corresponding mutational stage with conditional activation
     - wrap all bit-level HAVOC mutations into bit-level DY mutations:
       + most of them can be easily lifted to traces using the `expand_mutations` macro
       + others need custom lifting such as `BytesInsertCopyMutatorDY`
       + cross-over mutations cannot be lifted at all since we cannot make traces have the required traits. For those, we inline and adapt the mutation implementation.
     - Introduced `stage_bit` mutational stage using bit-level mutators applied in a controlled manner depending on executions and corpus size thresholds.
     - Reservoir: Enhance term filtering with additional constraints for terms with payloads

--- puffin/src/fuzzer/mutations.rs
+++ puffin/src/fuzzer/mutations.rs
@@ -24,6 +24,8 @@ pub struct MutationConfig {
     pub term_constraints: TermConstraints,
     pub with_bit_level: bool,
     pub with_dy: bool,
+    // Focus on one payload at a time for a whole StdMutationalStage
+    pub with_focus: bool,
 }
 
 impl Default for MutationConfig {
@@ -36,6 +38,7 @@ impl Default for MutationConfig {
             term_constraints: TermConstraints::default(),
             with_bit_level: true,
             with_dy: true,
+            with_focus: false,
         }
     }
 }
