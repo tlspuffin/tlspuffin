daf3425f026cedac029023e68049c906287f75f3
vdh-anssi <vincent.diemunsch@inria.fr>
2025-12-01 15:47:13 +0100

    Added an opc.tcp//localhost/4840 as default PUT

--- opcuapuffin/src/put_registry.rs
+++ opcuapuffin/src/put_registry.rs
@@ -3,10 +3,13 @@ use puffin::put_registry::PutRegistry;
 use crate::protocol::OpcuaProtocolBehavior;
 
 pub const OPEN62541: &str = "open62541";
+pub const OPC_TCP: &str = "opc.tcp";
 
 pub fn opcua_registry() -> PutRegistry<OpcuaProtocolBehavior> {
     PutRegistry::new(
-        [(OPEN62541, crate::harnesses::open62541::new_opcua_factory())],
-        OPEN62541,
+        [(OPEN62541, crate::puts::open62541::new_opcua_factory()),
+               (OPC_TCP, crate::puts::tcp::new_opcua_factory()),
+        ],
+        OPC_TCP,
     )
 }
--- opcuapuffin/src/puts/mod.rs
+++ opcuapuffin/src/puts/mod.rs
@@ -1,5 +1,6 @@
-// this module contains all harnesses of Protocol Under Test stacks.
+// this module contains all Protocol Under Test vendor libraries.
 
 //#[cfg_attr(feature = "open62541-binding", path = "open62541/mod.rs")]
 // #[cfg(open62541)]
 pub mod open62541;
+pub mod tcp;
