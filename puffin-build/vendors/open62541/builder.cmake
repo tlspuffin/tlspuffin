c9df611fa7edfb08483d516699fcaa9f229e2868
vdh-anssi <vincent.diemunsch@inria.fr>
2025-12-01 15:36:19 +0100

    Added OPC UA stack 'open62541' to puffin-build

---
+++ puffin-build/vendors/open62541/builder.cmake
@@ -0,0 +1,28 @@
+use_languages(C)
+
+cmake_builder(
+  TARGETS
+    install
+
+  CMAKE_FLAGS
+    -DUA_ARCHITECTURE=puffin
+    -DUA_BUILD_EXAMPLES=OFF
+    -DUA_ENABLE_ENCRYPTION=OPENSSL
+
+    # SANCOV
+    $<$<BOOL:${sancov}>:-fsanitize-coverage=trace-pc-guard>
+
+    # ASAN
+    $<$<BOOL:${asan}>:-fsanitize=address>
+    $<$<BOOL:${asan}>:-static-libsan>
+
+    # LLVM_COV
+    $<$<BOOL:${llvm_cov}>:-fprofile-instr-generate>
+    $<$<BOOL:${llvm_cov}>:-fcoverage-mapping>
+    $<$<BOOL:${llvm_cov}>:-O0>
+
+    # GCOV
+    $<$<BOOL:${gcov}>:-ftest-coverage>
+    $<$<BOOL:${gcov}>:-fprofile-arcs>
+    $<$<BOOL:${gcov}>:-O0>
+)
