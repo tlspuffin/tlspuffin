a6b7ba83c0ffa0edabcb674a466c7ee069c4d89a
vdh-anssi <vincent.diemunsch@inria.fr>
2026-01-07 10:29:00 +0100

    Corrected a memory leak due to txBuffer not being properly deallocated

--- opcuapuffin/harness/open62541/src/eventloop_puffin.c
+++ opcuapuffin/harness/open62541/src/eventloop_puffin.c
@@ -556,15 +556,9 @@ UA_EventLoopPuffin_freeNetworkBuffer(UA_ConnectionManager *cm,
                                     uintptr_t connectionId,
                                     UA_ByteString *buf) {
     UA_PuffinConnectionManager *pcm = (UA_PuffinConnectionManager*)cm;
-    if(pcm->txBuffer.data == buf->data) {
-        UA_LOG_DEBUG(pcm->cm.eventSource.eventLoop->logger, UA_LOGCATEGORY_NETWORK,
-            "TCP %u\t| Free txBuffer (init) of size %u, at %p", (unsigned)connectionId, buf->length, buf->data);
-        UA_ByteString_init(buf);
-    } else {
-        UA_LOG_DEBUG(pcm->cm.eventSource.eventLoop->logger, UA_LOGCATEGORY_NETWORK,
-            "TCP %u\t| Free txBuffer (clear) of size %u, at %p", (unsigned)connectionId, buf->length, buf->data);
-        UA_ByteString_clear(buf);
-    }
+    UA_LOG_DEBUG(pcm->cm.eventSource.eventLoop->logger, UA_LOGCATEGORY_NETWORK,
+        "TCP %u\t| Free txBuffer (clear) of size %u, at %p", (unsigned)connectionId, buf->length, buf->data);
+    UA_ByteString_clear(buf);
 }
 
 UA_StatusCode
