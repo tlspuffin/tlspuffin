948425a52e5e9c246f296dfe9473be1d5dafd4f2
vdh-anssi <vincent.diemunsch@inria.fr>
2026-01-19 18:30:11 +0100

    tools/profile/exec_perf.sh is able to work with any protocol

--- tools/profile/exec_perf.sh
+++ tools/profile/exec_perf.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+# Usage :
+# ./exec_perf.sh protocol seeds/seed_a.trace [--release]
 
-tlspuffin_dir="$(dirname "$0")/../.."
-to_execute="$tlspuffin_dir/$1"
+
+puffins_dir="$(dirname "$0")/../.."
+protocol=$1
+trace="$puffins_dir/$2"
 # If an option --release is passed, set the variable target to "release"
-if [[ "$2" == "--release" ]]; then
+if [[ "$3" == "--release" ]]; then
     target="release"
 else
     target="debug"
 fi
-tlspuffin="${tlspuffin_dir}/target/${target}/tlspuffin"
-echo "To execute: $to_execute"
+my_puffin="${puffins_dir}/target/${target}/${protocol}puffin"
+echo "To execute: $trace"
 echo "Puffin binary was built at:"
-stat -c "%w %n" ${tlspuffin}
+stat -c "%w %n" ${my_puffin}
 
-perf record --call-graph dwarf -- ${tlspuffin} execute ${to_execute}
-perf script | "${tlspuffin_dir}/tools/profile/stackcollapse-perf.pl" \
-            | "${tlspuffin_dir}/tools/profile/rust-unmangle" \
-            | "${tlspuffin_dir}/tools/profile/flamegraph.pl" > flame.svg
+perf record --call-graph dwarf -- ${my_puffin} execute ${trace}
+perf script | "${puffins_dir}/tools/profile/stackcollapse-perf.pl" \
+            | "${puffins_dir}/tools/profile/rust-unmangle" \
+            | "${puffins_dir}/tools/profile/flamegraph.pl" > flame.svg
 
 # xdg-open flame.svg
