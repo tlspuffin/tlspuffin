89f8db351f9d29d560d6e39b8944bf6cb5b7c7f0
vdh-anssi <vincent.diemunsch@inria.fr>
2026-01-22 16:46:47 +0100

    First try to add S2OPC as a vendor library

---
+++ puffin-build/vendors/s2opc/builder.cmake
@@ -0,0 +1,21 @@
+use_languages(C)
+
+#patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Modifications-of-CLI-client-and-server.patch)
+
+cmake_builder(
+  TARGETS
+    install
+
+  CMAKE_FLAGS
+    -DENABLE_SAMPLES=OFF
+    -DENABLE_TESTING=OFF
+    -DS2OPC_CLIENTSERVER_ONLY=ON
+    -DS2OPC_CRYPTO_MBEDTLS=ON
+    -DWITH_ASAN=OFF # Mutually exclusive with source coverage why???
+    -DWITH_CLANG_SOURCE_COVERAGE=ON
+
+  CFLAGS
+    -g
+    -fPIC
+    -fvisibility=hidden
+)
