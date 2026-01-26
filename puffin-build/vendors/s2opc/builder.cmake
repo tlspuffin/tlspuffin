d849e31aeab2c44b51c367b305405442a4d76785
vdh-anssi <vincent.diemunsch@inria.fr>
2026-01-26 14:55:11 +0100

    First try to add S2OPC as a vendor library

--- puffin-build/vendors/s2opc/builder.cmake
+++ puffin-build/vendors/s2opc/builder.cmake
@@ -2,6 +2,20 @@ use_languages(C)
 
 #patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Modifications-of-CLI-client-and-server.patch)
 
+include(FetchContent)
+
+set(MBEDTLS_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(MBEDTLS_BUILD_PROGRAMS OFF CACHE BOOL "" FORCE)
+set(MBEDTLS_TLS1_3 ON CACHE BOOL "" FORCE)
+
+FetchContent_Declare(
+  mbedtls
+  GIT_REPOSITORY https://github.com/Mbed-TLS/mbedtls.git
+  GIT_TAG v3.6.5
+  )
+
+FetchContent_MakeAvailable(mbedtls)
+
 cmake_builder(
   TARGETS
     install
@@ -14,6 +28,10 @@ cmake_builder(
     -DS2OPC_CRYPTO_MBEDTLS=ON
     -DWITH_ASAN=OFF # Mutually exclusive with source coverage why???
     -DWITH_CLANG_SOURCE_COVERAGE=ON
+    -DMBEDTLS_INCLUDE_DIR=${mbedtls_SOURCE_DIR}/include
+    -DMBEDTLS_LIBRARY=${mbedtls_SOURCE_DIR}/library
+    -DMBEDX509_LIBRARY=${mbedtls_SOURCE_DIR}/library
+    -DMBEDCRYPTO_LIBRARY=${mbedtls_SOURCE_DIR}/library
 
   CFLAGS
     -g
