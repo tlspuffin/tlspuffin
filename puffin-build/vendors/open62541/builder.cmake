db44841ddc2af9dae74ab9571b2fe68aa11b398d
vdh-anssi <vincent.diemunsch@inria.fr>
2026-03-03 16:41:52 +0100

    With a patch, fixed a bug in open62541 that prevented to display the ClientUserId of a user certificate

--- puffin-build/vendors/open62541/builder.cmake
+++ puffin-build/vendors/open62541/builder.cmake
@@ -1,8 +1,10 @@
 use_languages(C)
 
 patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Modifications-of-CLI-client-and-server.patch)
-#patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Instrumentation-to-debug.patch)
+patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Instrumentation-to-debug.patch)
 #patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Memory-leak.patch)
+patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Bug-fix-of-ClientUserId.patch)
+
 
 cmake_builder(
   TARGETS
