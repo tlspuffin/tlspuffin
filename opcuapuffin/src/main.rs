8f75c280752d83a1cbf074f22410d297cb1a358a
vdh-anssi <vincent.diemunsch@inria.fr>
2026-05-22 16:38:59 +0200

    Added function atexit from stdlib to main.rs, to support GCOV

--- opcuapuffin/src/main.rs
+++ opcuapuffin/src/main.rs
@@ -8,3 +8,36 @@ pub fn main() -> ExitCode {
         opcua_registry(),
     )
 }
+
+// This fix is only for GCOV and LLVM_COV
+// ----------------------------------------------------------------------------
+// The linker error "undefined reference to 'atexit'" occurs when mixing AddressSanitizer (ASan), GCOV, and Rust's -nodefaultlibs.
+// The Problem:
+//    GCOV needs to register a "save-on-exit" handler via 'atexit' in function `llvm_gcov_init'.
+//    However, because the PUT is built without its own standard library to avoid symbol conflicts, the GCOV runtime couldn't find the atexit symbol.
+// The Fix:
+//    A manual implementation of atexit in Rust that calls the lower-level GLIBC function __cxa_atexit.
+//     Why __cxa_atexit? Calling libc::atexit directly would have caused an infinite recursion (stack overflow) because our wrapper was named atexit
+#[no_mangle]
+pub unsafe extern "C" fn atexit(f: extern "C" fn()) -> libc::c_int {
+    extern "C" {
+        fn __cxa_atexit(
+            cb: extern "C" fn(*mut libc::c_void),
+            arg: *mut libc::c_void,
+            dso: *mut libc::c_void,
+        ) -> libc::c_int;
+    }
+
+    extern "C" fn atexit_wrapper(arg: *mut libc::c_void) {
+        unsafe {
+            let f: extern "C" fn() = std::mem::transmute(arg);
+            f();
+        }
+    }
+
+    __cxa_atexit(
+        atexit_wrapper,
+        f as *mut libc::c_void,
+        std::ptr::null_mut(),
+    )
+}
