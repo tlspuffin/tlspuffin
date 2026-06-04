4edcc764cc92bf7df188b2837674f77ca6a06814
nbaffou <natael.baffou@inria.fr>
2026-06-04 11:20:19 +0200

    TLS:PUT:libressl: Adds patch validation to abort on failure and not have a silent failure

--- puffin-build/vendors/libressl/patch_insecure.cmake
+++ puffin-build/vendors/libressl/patch_insecure.cmake
@@ -3,5 +3,16 @@
 # (extracted_early, extracted_handshake, extracted_master) after derivation,
 # allowing the fuzzer harness to read them for security claims.
 file(READ "${FILE}" content)
+
+set(before "${content}")
+
 string(REPLACE "secrets->init_done = 1;" "secrets->init_done = 1; secrets->insecure = 1;" content "${content}")
+
+if("${content}" STREQUAL "${before}")
+  message(FATAL_ERROR
+    "patch_insecure: replacement had no effect in '${FILE}'.\n"
+    "The LibreSSL source may have changed (renamed variable, restructured init logic, "
+    "or changed whitespace). Update this patch to match the new source.")
+endif()
+
 file(WRITE "${FILE}" "${content}")
