4b7b43b49afc5ebdade829c39990926a254fd9f2
pepinpig <qsdfghjklmtechno0@gmail.com>
2026-07-03 13:20:39 +0200

    feat : claim_feedback active

--- puffin/src/fuzzer/feedback/mod.rs
+++ puffin/src/fuzzer/feedback/mod.rs
@@ -79,8 +79,8 @@ where
     let tracked_profile_feedback = TrackingFeedbackWrapper::new(profile_feedback);
     let tracked_term_feedback = TrackingFeedbackWrapper::new(term_feedback);
     let feedback = feedback_or!(
-        tracked_term_feedback,
-        //tracked_claim_feedback,
+        //tracked_term_feedback,
+        tracked_claim_feedback,
         //tracked_profile_feedback,
         //tracked_semantic_edge_feedback,
         map_feedback,
