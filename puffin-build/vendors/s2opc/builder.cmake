95e3cafb7753b474aa1d751dc6ec3800b8bb17c8
vdh-anssi <vincent.diemunsch@inria.fr>
2026-07-28 18:12:43 +0200

    Normalized puffin-build presets

--- puffin-build/vendors/s2opc/builder.cmake
+++ puffin-build/vendors/s2opc/builder.cmake
@@ -13,9 +13,9 @@ cmake_builder(
     -DENABLE_SAMPLES=OFF
     -DENABLE_TESTING=OFF # To remove the dependency to Check lib.
     -DFETCHCONTENT_QUIET=OFF
-    -DS2OPC_NANO_PROFILE=ON
     -DS2OPC_CLIENTSERVER_ONLY=ON
     -DS2OPC_CRYPTO_MBEDTLS=ON
+    -DS2OPC_NANO_PROFILE=ON
     -DWITH_ASAN=OFF # Mutually exclusive with source coverage why???
     -DWITH_CLANG_SOURCE_COVERAGE=OFF
     -DWITH_PYS2OPC=OFF #No Python and hopefully no need of expat lib.
