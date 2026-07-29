47b366acc70cef2737e7416e6e29b0e63f7330c0
vdh-anssi <vincent.diemunsch@inria.fr>
2026-07-29 18:19:56 +0200

    Added the ability to inject a simple buffer overflow

--- puffin-build/vendors/open62541/builder.cmake
+++ puffin-build/vendors/open62541/builder.cmake
@@ -2,9 +2,10 @@ use_languages(C)
 
 patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Modifications-of-CLI-client-and-server.patch)
 patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Instrumentation-to-debug.patch)
-#patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Memory-leak.patch)
 patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Bug-fix-of-ClientUserId.patch)
 
+# Bug injection:
+#patch(FILE ${CMAKE_CURRENT_LIST_DIR}/patches/Buffer_overflow.patch)
 
 cmake_builder(
   TARGETS
