843d3b6dbfbd8746f627504a708d8df201fe9a69
vdh-anssi <vincent.diemunsch@inria.fr>
2026-08-24 19:18:51 +0200

    A new tool to filter Puffin's output

--- opcuapuffin/evaluation/filter_output.py
+++ opcuapuffin/evaluation/filter_output.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-#import numpy as np
 import sys
 
 def filter_stdout():
    with open("measurements.txt", "w", encoding="utf-8") as file:
-      file.write("measurements = [\n")
       current_size = 0
       try:
-        # Lit l'entrée standard ligne par ligne en temps réel
+        # Read standard input line by line
         for line in sys.stdin:
             if "[Stats] (GLOBAL) clients" in line:
                 start = line.index("corpus:") + 7
                 stop  = line[start: -1].index(",") + start
-                size  = int(line[start:stop]) 
-                if size > current_size + current_size / 10:
+                size  = int(line[start:stop])
+                if size > current_size + current_size / 100:
                      current_size = size
-                     # Affiche la ligne correspondante sans ajouter de saut de ligne doublon
+                     # Displays the corresponding line without adding a duplicate line break
                      sys.stdout.write(line)
-                     # Force le vidage du tampon pour un affichage instantané
+                     # Forces the buffer to be flushed for instant display
                      sys.stdout.flush()
-                     file.write('   ("' + line[0:19] + '", ' + str(size) + "),\n")
+                     # Writing in parallel to the file that will be used for post-processing
+                     file.write(line[0:19] + ', ' + str(size) +"\n")
+                     file.flush()
       except KeyboardInterrupt:
-         # Permet de quitter proprement avec Ctrl+C
-         file.write("]\n")
+         # To exit cleanly with Ctrl+C
          sys.exit(0)
 
 if __name__ == "__main__":
